824783f7d49dc95433d1553e2d371af0801904dd
Nikita <kitathedeveloper@gmail.com>
2025-04-23 22:29:00 +0500

    df

--- src/config.py
+++ src/config.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 
-FOLDER_ID = "1oKX-CjKn1zKN-OwPK77lXWcprRY9N_M5"
-RAW_FOLDER_ID = "1XUffNjN0TXomLdaBXsqTM1k5xB9-mgZe"
-PROCESSED_FOLDER_ID = "1xy6OqnmZsARbSO-rh-Z4FeNstfx_nB5N"
+RAW_FOLDER_ID = ""
+PROCESSED_FOLDER_ID = ""
 
 DATA_FOLDER = Path(__file__).resolve().parent.parent / "data"
 
